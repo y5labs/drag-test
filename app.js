4fb49ca0cb37872115d9d68f59e9a69a5b272b83
Thomas Coats <github@voodoolabs.net>
2020-05-12 22:35:30 +1200

    wip

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
 import component from './component'
 import putty from './putty'
-import { format, utcToZonedTime } from 'date-fns-tz'
 import { getUnixTime, parseISO } from 'date-fns'
-import locale from 'date-fns/locale/en-GB'
-const fmt = (d, timeZone) =>
-  format(
-    utcToZonedTime(d, timeZone),
-    'yyyy-MM-dd\'T\'HH:mm:ss (zzz)',
-    { timeZone, locale })
 
-import dsl from './lib/dsl'
-import schedule from './lib/schedule'
 
-const parsed = dsl(`
-
-  start: now/M
-  end: now+4M/M
-
-  // the first tuesday of every month
-  result:
-  + interval(now/M, +7d, 1M)
-
-`,
-'America/New_York')
-console.log(parsed.constants)
-const segments = schedule(
-  parsed.schedules,
-  parsed.schedules.result,
-  {
-    start: parsed.constants.start,
-    end: parsed.constants.end
-  },
-  'America/New_York')
-for (let segment of segments) {
-  console.log(`${fmt(segment.start, 'America/New_York')} — ${fmt(segment.end, 'America/New_York')}`)
+const visible = {
+  start: getUnixTime(parseISO('2020-05-10T12:00:00Z')),
+  end: getUnixTime(parseISO('2020-05-11T12:00:00Z'))
 }
-
+const secondsInAPixel = 3 * 60
+const x2p = d => (d - visible.start) / secondsInAPixel
+const p2x = x => (x * secondsInAPixel) + visible.start
 
 
 const dataset = [
@@ -52,28 +26,69 @@ const dataset = [
     end: getUnixTime(parseISO('2020-05-11T05:00:00Z'))
   }
 ]
-const visible = {
-  start: getUnixTime(parseISO('2020-05-10T12:00:00Z')),
-  end: getUnixTime(parseISO('2020-05-11T12:00:00Z'))
-}
-const secondsInAPixel = 3 * 60
 
-const x = d => (d - visible.start) / secondsInAPixel
-
-const edges = Array.from(
+const getEdges = dataset => Array.from(
   dataset.reduce((res, d, i) => {
-    if (res.has(d.start)) res.get(d.start).start = i
-    else res.set(d.start, { start: i, end: null })
-    res.set(d.end, { start: null, end: i })
+    if (res.has(d.start)) res.get(d.start).startindex = i
+    else res.set(d.start, { startindex: i, endindex: null })
+    res.set(d.end, { startindex: null, endindex: i })
     return res
   }, new Map())
   .entries(),
-  d => ({ x: x(d[0]), ...d[1] }))
+  d => ({ start: d[0], ...d[1] }))
 
-console.log(dataset, edges)
-console.log(
-  dataset.map(d => ({ start: x(d.start), end: x(d.end) }))
-)
+const operation = {
+  type: 'resize start',
+  startindex: 0,
+  endindex: null,
+  delta: 5 * secondsInAPixel
+}
+
+const result = ((input, operation) => {
+  const dataset = input.slice()
+  if (operation.type == 'resize start') {
+    const selected = dataset[operation.startindex]
+    let { start, end } = selected
+    start += operation.delta
+    if (start > end) [start, end] = [end, start]
+    dataset.splice(operation.startindex, 1)
+    return {
+      dataset,
+      start: selected,
+      current: { start, end },
+      delta: {
+        start: start - selected.start,
+        end: end - selected.end
+      }
+    }
+  }
+  if (operation.type == 'resize end') {
+    const selected = dataset[operation.endindex]
+    let { start, end } = selected
+    end += operation.delta
+    if (start > end) [start, end] = [end, start]
+    dataset.splice(operation.endindex, 1)
+    return {
+      dataset,
+      start: selected,
+      current: { start, end },
+      delta: {
+        start: start - selected.start,
+        end: end - selected.end
+      }
+    }
+  }
+})(dataset, operation)
+
+console.log(result)
+
+
+
+
+const edges = getEdges(dataset)
+
+console.log(dataset.map(d => ({ x1: x2p(d.start), x2: x2p(d.end), ...d })))
+console.log(edges.map(d => ({ x: x2p(d.start), ...d })))
 
 
 
