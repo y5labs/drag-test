622552e5071f03a46cc7dc06b3a70b189345d338
Thomas Coats <github@voodoolabs.net>
2020-05-14 14:56:57 +1200

    labels

--- app.js
+++ app.js
@@ -1,37 +1,85 @@
 import component from './component'
-import putty from './putty'
-import { getUnixTime, parseISO } from 'date-fns'
-import { rangeSubtract, getEdges, operate } from './lib/range'
+import putty from 'vue-putty'
+import { getUnixTime, fromUnixTime, parseISO } from 'date-fns'
+import { rangeSubtract } from 'date-fns-lost'
 
+import locale from 'date-fns/locale/en-GB'
+import { format, utcToZonedTime, toDate } from 'date-fns-tz'
+
+const fmt = (d, timeZone) =>
+  format(utcToZonedTime(d, timeZone), 'HH:mm', { timeZone, locale })
+
+
+const getEdges = dataset => Array.from(
+  dataset.reduce((res, d, i) => {
+    if (res.has(d.start)) res.get(d.start).startindex = i
+    else res.set(d.start, { startindex: i, endindex: null })
+    res.set(d.end, { startindex: null, endindex: i })
+    return res
+  }, new Map())
+  .entries(),
+  d => ({ start: d[0], ...d[1] }))
+
+const operate = (input, operation, quant = ts => ts) => {
+  const dataset = input.slice()
+  if (operation.type == 'new') {
+    let start = operation.start
+    let end = operation.start + operation.delta
+    if (start > end) [start, end] = [end, start]
+    const current = quant({ start, end })
+    return {
+      dataset: dataset.map(d => rangeSubtract(d, current)).flat(),
+      selected: current,
+      current
+    }
+  }
+  const selected = dataset[operation.index]
+  let { start, end } = selected
+  if (operation.type == 'resize start') start += operation.delta
+  if (operation.type == 'resize end') end += operation.delta
+  if (operation.type == 'move') {
+    start += operation.delta
+    end += operation.delta
+  }
+  if (start > end) [start, end] = [end, start]
+  const current = quant({ start, end })
+  dataset.splice(operation.index, 1)
+  return {
+    dataset: dataset.map(d => rangeSubtract(d, current)).flat(),
+    selected,
+    current
+  }
+}
 
 const visible = {
-  start: getUnixTime(parseISO('2020-04-03T12:00:00Z')),
-  end: getUnixTime(parseISO('2020-04-04T12:00:00Z'))
+  start: getUnixTime(toDate('2020-04-05T00:00:00', 'Pacific/Auckland')),
+  end: getUnixTime(toDate('2020-04-06T00:00:00', 'Pacific/Auckland'))
 }
 const secondsInAPixel = 2 * 60
 const ts2px = d => (d - visible.start) / secondsInAPixel
 const px2ts = ts => (ts * secondsInAPixel) + visible.start
 const range2px = d => ({ ts1: ts2px(d.start), ts2: ts2px(d.end), ...d })
-const smallestincrement = 6 * 60
+const smallestincrement = 5 * 60
+const quantts = ts => Math.round(ts / smallestincrement) * smallestincrement
 const quant = ({ start, end }) => {
-  start = Math.round(start / smallestincrement) * smallestincrement
-  end = Math.round(end / smallestincrement) * smallestincrement
+  start = quantts(start)
+  end = quantts(end)
   if (start == end) end += smallestincrement
   return { start, end }
 }
 
 let dataset = [
   {
-    start: getUnixTime(parseISO('2020-04-03T23:00:00Z')),
-    end: getUnixTime(parseISO('2020-04-04T00:00:00Z'))
+    start: getUnixTime(toDate('2020-04-05T11:00:00', 'Pacific/Auckland')),
+    end: getUnixTime(toDate('2020-04-05T12:00:00', 'Pacific/Auckland'))
   },
   {
-    start: getUnixTime(parseISO('2020-04-04T01:00:00Z')),
-    end: getUnixTime(parseISO('2020-04-04T04:00:00Z'))
+    start: getUnixTime(toDate('2020-04-05T15:00:00', 'Pacific/Auckland')),
+    end: getUnixTime(toDate('2020-04-05T16:00:00', 'Pacific/Auckland'))
   },
   {
-    start: getUnixTime(parseISO('2020-04-04T04:00:00Z')),
-    end: getUnixTime(parseISO('2020-04-04T05:00:00Z'))
+    start: getUnixTime(toDate('2020-04-05T16:00:00', 'Pacific/Auckland')),
+    end: getUnixTime(toDate('2020-04-05T18:00:00', 'Pacific/Auckland'))
   }
 ]
 
@@ -41,6 +89,7 @@ export default component({
   render: (h, { props, hub, state, route, router }) => {
     if (!props.operation) {
       const ds = dataset.map(range2px)
+      const selected = props.selectedindex != null ? dataset[props.selectedindex] : null
       const edges = getEdges(dataset).map(d => ({ ts: ts2px(d.start), ...d }))
       const findEdge = ts => {
         for (const d of edges) if (Math.abs(d.ts - ts) <= 4) return d
@@ -65,79 +114,92 @@ export default component({
       return h('#root', [
         h('.area', { style: { width: `${ts2px(visible.end)}px` } }, [
           h(putty, {
-            attrs: {
-              hub: hub.child({
-                start: p => {
-                  const ts = p.start[0]
-                  const edge = findEdge(ts)
-                  if (edge != null) {
-                    if (edge.endindex != null) {
-                      operation = { type: 'resize end', index: edge.endindex, delta: 0 }
-                      hub.emit('update', { operation, selectedindex: null })
-                      window.addEventListener('keypress', cancelifesc)
-                    }
-                    else if (edge.startindex != null) {
-                      operation = { type: 'resize start', index: edge.startindex, delta: 0 }
-                      hub.emit('update', { operation, selectedindex: null })
-                      window.addEventListener('keypress', cancelifesc)
-                    }
-                    return
-                  }
-                  const index = findRangeIndex(ts)
-                  if (index != null) {
-                    operation = { type: 'move', index, delta: 0 }
+            on: {
+              start: p => {
+                const ts = p.start[0]
+                const edge = findEdge(ts)
+                if (edge != null) {
+                  if (edge.endindex != null) {
+                    operation = { type: 'resize end', index: edge.endindex, delta: 0 }
                     hub.emit('update', { operation, selectedindex: null })
                     window.addEventListener('keypress', cancelifesc)
-                    return
                   }
-                  operation = {  type: 'new', start: px2ts(ts), delta: 0 }
+                  else if (edge.startindex != null) {
+                    operation = { type: 'resize start', index: edge.startindex, delta: 0 }
+                    hub.emit('update', { operation, selectedindex: null })
+                    window.addEventListener('keypress', cancelifesc)
+                  }
+                  return
+                }
+                const index = findRangeIndex(ts)
+                if (index != null) {
+                  operation = { type: 'move', index, delta: 0 }
                   hub.emit('update', { operation, selectedindex: null })
                   window.addEventListener('keypress', cancelifesc)
-                },
-                move: p => {
-                  if (!operation) return
-                  operation.delta = p.delta[0] * secondsInAPixel
-                  hub.emit('update', { operation, selectedindex: null })
-                },
-                end: p => {
-                  if (!operation) return
-                  const result = operate(dataset, operation, quant)
-                  dataset = result.dataset
-                  let selectedindex = null
-                  if (operation.type == 'resize start'
-                    || operation.type == 'resize end'
-                    || operation.type == 'move'
-                    || operation.type == 'new') {
-                    dataset.push(result.current)
-                    selectedindex = dataset.length - 1
-                  }
-                  hub.emit('update', { operation: null, selectedindex })
-                  window.removeEventListener('keypress', cancelifesc)
-                  document.body.style.cursor = 'auto'
-                },
-                tap: p => {
-                  const selectedindex = findRangeIndex(p[0])
-                  if (selectedindex != null)
-                    hub.emit('update', { selectedindex })
-                  else
-                    hub.emit('update', { selectedindex: null })
-                },
-                leave: () => document.body.style.cursor = 'auto',
-                hover: p => {
-                  const ts = p.current[0]
-                  if (findEdge(ts) != null)
-                    document.body.style.cursor = 'col-resize'
-                  else if (findRangeIndex(ts) != null)
-                    document.body.style.cursor = 'ew-resize'
-                  else document.body.style.cursor = 'crosshair'
+                  return
                 }
-              })
+                operation = {  type: 'new', start: px2ts(ts), delta: 0 }
+                hub.emit('update', { operation, selectedindex: null })
+                window.addEventListener('keypress', cancelifesc)
+              },
+              move: p => {
+                if (!operation) return
+                operation.delta = p.delta[0] * secondsInAPixel
+                hub.emit('update', { operation, selectedindex: null })
+              },
+              end: p => {
+                if (!operation) return
+                const result = operate(dataset, operation, quant)
+                dataset = result.dataset
+                let selectedindex = null
+                if (operation.type == 'resize start'
+                  || operation.type == 'resize end'
+                  || operation.type == 'move'
+                  || operation.type == 'new') {
+                  dataset.push(result.current)
+                  selectedindex = dataset.length - 1
+                }
+                hub.emit('update', { operation: null, selectedindex })
+                window.removeEventListener('keypress', cancelifesc)
+                document.body.style.cursor = 'auto'
+              },
+              tap: p => {
+                const selectedindex = findRangeIndex(p[0])
+                if (selectedindex != null)
+                  hub.emit('update', { selectedindex })
+                else
+                  hub.emit('update', { selectedindex: null })
+              },
+              leave: () => {
+                document.body.style.cursor = 'auto'
+                hub.emit('update', { hoverts: null })
+              },
+              hover: p => {
+                const px = p.current[0]
+                if (findEdge(px) != null)
+                  document.body.style.cursor = 'col-resize'
+                else if (findRangeIndex(px) != null)
+                  document.body.style.cursor = 'ew-resize'
+                else
+                  document.body.style.cursor = 'crosshair'
+                hub.emit('update', { hoverts: px2ts(px) })
+              }
             }
           }),
           ...ds.map((d, i) => h('.box', {
             class: { selected: i == props.selectedindex },
             style: { left: `${d.ts1}px`, width: `${d.ts2 - d.ts1}px` }
-          }))
+          })),
+          ...(selected ? [
+            h('.start', { style: { left: `${ts2px(selected.start)}px` } },
+              fmt(fromUnixTime(selected.start), 'Pacific/Auckland')),
+            h('.end', { style: { left: `${ts2px(selected.end)}px`  } },
+              fmt(fromUnixTime(selected.end), 'Pacific/Auckland'))
+          ]
+          : [
+            props.hoverts && h('.hover', { style: { left: `${ts2px(props.hoverts)}px` } },
+            fmt(fromUnixTime(quantts(props.hoverts)), 'Pacific/Auckland'))
+          ])
         ])
       ])
     }
@@ -156,7 +218,11 @@ export default component({
             left: `${current.ts1}px`,
             width: `${current.ts2 - current.ts1}px`
           }
-        })
+        }),
+        h('.start', { style: { left: `${ts2px(result.current.start)}px` } },
+          fmt(fromUnixTime(result.current.start), 'Pacific/Auckland')),
+        h('.end', { style: { left: `${ts2px(result.current.end)}px` } },
+          fmt(fromUnixTime(result.current.end), 'Pacific/Auckland'))
       ])
     ])
   }
